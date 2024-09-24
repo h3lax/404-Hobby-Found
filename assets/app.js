5962b3f879f6201b0e2b8989ec8e2008675ec351
Michael <michael.picard14@gmail.com>
2024-09-24 14:15:26 +0200

    Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,10 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+// Import JQuery
+
+import $ from 'jquery';
+
 // start the Stimulus application
 import './bootstrap';
 
---
+++ src/Controller/MyClubsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MyClubsController extends AbstractController
+{
+    #[Route('/', name: 'app_my_clubs')]
+    public function index(): Response
+    {
+        return $this->render('my_clubs/index.html.twig', [
+            'controller_name' => 'MyClubsController',
+        ]);
+    }
+}
